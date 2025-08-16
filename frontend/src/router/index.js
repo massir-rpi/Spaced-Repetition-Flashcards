9422d5368bb3694900f436ef197cad7fd9014131
Richard Massimilla <richiesmassi@gmail.com>
2025-08-15 23:54:48 -0400

    Replace hashhistory with webhistory

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { useAuth } from '../composables/useAuth.js';
 
 import StudyView from '../views/StudyView.vue';
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
